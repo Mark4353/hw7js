8e49208cee92a6d4cd0a5695b56cc2dc52dff63d
der1x <130235333+Mark4353@users.noreply.github.com>
2025-05-04 14:59:02 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import Handlebars from 'handlebars';
-import products  from './data.json';
-import templateText from './template.hbs'; 
-
-const html = templateText({products});
-
-document.body.innerHTML = html;
