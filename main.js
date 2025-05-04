fc813cafa8d9a5fce8d6862e79030cdb4387d0b1
der1x <130235333+Mark4353@users.noreply.github.com>
2025-05-04 11:09:13 +0300

    fix

--- main.js
+++
@@ -1,8 +0,0 @@
-import Handlebars from "handlebars";
-import { products } from "./data.js";
-import templateSource from "./template.hbs";
-
-const template = Handlebars.compile(templateSource);
-const markup = template(products);
-
-document.body.innerHTML = markup;
