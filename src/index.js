2ed7fa191763540fa6a6f389dd578462b1c61dfc
der1x <130235333+Mark4353@users.noreply.github.com>
2025-05-04 14:36:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Handlebars from 'handlebars';
 import products  from './data.json';
 import templateText from './template.hbs'; 
 
-// const template = Handlebars.compile(templateText);
 const html = templateText({products});
 
 document.body.innerHTML = html;
